058d692ade4a2f8c12abce101f2e7dd1db11264a
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2019-02-24 18:47:39 +0530

    addec comments for graphql controller

--- example/src/controllers/GraphQlController.ts
+++ example/src/controllers/GraphQlController.ts
@@ -1,24 +1,32 @@
-import { textResult, HTTP_METHOD, DefaultWorker, Worker } from "fortjs";
+import { HTTP_METHOD, DefaultWorker, Worker } from "fortjs";
 import { FortGraphQl } from "fort-graphql";
-import { buildSchema } from "graphql";
+import { graphqlSchema } from "../graphql_schema";
 
-const schema = `
-type Query {
-  hello: String
-}
-`;
+
 export class GraphQlController extends FortGraphQl {
     rootValue = { hello: () => 'Hello world!' };
 
     constructor() {
-        super(schema);
+        super(graphqlSchema);
     }
 
+    /**
+     * This method will be used to process graphql query 
+     *
+     * @returns
+     * @memberof GraphQlController
+     */
     @DefaultWorker([HTTP_METHOD.Get, HTTP_METHOD.Post])
     async  default() {
         return this.processGraphQl();
     }
 
+    /**
+     * This method will return graphiql 
+     *
+     * @returns
+     * @memberof GraphQlController
+     */
     @Worker()
     async  graphiql() {
         return this.getGraphiqlUi();
---
+++ example/src/graphql_schema.ts
@@ -0,0 +1,5 @@
+export const graphqlSchema = `
+type Query {
+  hello: String
+}
+`;
